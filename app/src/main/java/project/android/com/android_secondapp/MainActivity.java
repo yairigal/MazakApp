da26219e0917f82438a61f0143dba5b7284c2e89
Yair_Yigal <yigalyairn@gmail.com>
2016-12-18 17:55:29 +0200

    testing broadcast reciever works

--- app/src/main/java/project/android/com/android_secondapp/MainActivity.java
+++ app/src/main/java/project/android/com/android_secondapp/MainActivity.java
@@ -1,7 +1,10 @@
 package project.android.com.android_secondapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button broadcast = (Button) findViewById(R.id.button);
+        broadcast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                broadcastIntent(broadcast);
+            }
+        });
+    }
+
+    public void broadcastIntent(View view) {
+        Intent intent = new Intent();
+        intent.setAction("com.project.CHECK_DATABASE");
+        sendBroadcast(intent);
     }
 }
